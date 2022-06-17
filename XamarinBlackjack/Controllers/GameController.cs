530143056aa45192e0d12a97236b23ee44709324
TheFernandoM <molinaf@sheridancollege.ca>
2022-06-17 15:34:52 -0400

    Created GameController and its interface IGameController

--- XamarinBlackjack/Controllers/GameController.cs
+++ XamarinBlackjack/Controllers/GameController.cs
@@ -1,13 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using XamarinBlackjack.Enums;
+using XamarinBlackjack.Models;
+
 namespace XamarinBlackjack.Controllers
 {
     /// <summary>
     /// Game logic goes here
     /// </summary>
-    public class GameController
+    public class GameController :IGameController
     {
-        public GameController()
+        public List<Deck> Decks { get; set; }
+
+        public void ResetGame()
         {
+            throw new NotImplementedException();
+        }
+
+        public void SetupGame()
+        {
+            Decks.Add(new Deck());
+        }
+
+        public void StartGame()
+        {
+            throw new NotImplementedException();
+        }
+        public int CalculateHandValue(Hand hand)
+        {
+            foreach(var deck in Decks)
+            {
+                foreach(var card in deck.Cards)
+                {
+                    switch (card)
+                    {
+                        default:
+                            break;
+                        //TO DO
+                    }
+                }
+            }
+            return -1; // TO DO
+        }
+
+        public void EnactPlayerAction(PlayerActionEnum playerAction)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ XamarinBlackjack/Controllers/IGameController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using XamarinBlackjack.Enums;
+using XamarinBlackjack.Models;
+
+namespace XamarinBlackjack.Controllers
+{
+    public interface IGameController
+    {
+        public void SetupGame();
+
+        public void ResetGame();
+
+        public void StartGame();
+
+        public int CalculateHandValue(Hand hand);
+
+        public void EnactPlayerAction(PlayerActionEnum playerAction);
+    }
+}
