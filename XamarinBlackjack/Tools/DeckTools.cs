ef430ff5d86f5373dc549478412b7ff169907052
TheFernandoM <molinaf@sheridancollege.ca>
2022-06-19 03:04:39 -0400

    Remove DeckShuffler, create DeckTools and HandTools

---
+++ XamarinBlackjack/Tools/DeckTools.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using XamarinBlackjack.Enums;
+using XamarinBlackjack.Models;
+
+namespace XamarinBlackjack.Tools
+{
+    public static class DeckTools
+    {
+        /// <summary>
+        /// Shuffles a Deck of Cards. Runtime is O(n).
+        /// <br></br>
+        /// This implementation is based on
+        /// Wikipedia's  pseudocode description of the modern version of the
+        /// Fisher-Yates shuffle.
+        ///
+        /// Author: Fernando Molina
+        /// </summary>
+        public static Deck ShuffleDeck(Deck deck)
+        {
+            Random rand = new Random();
+            int deckSize = deck.Cards.Count;
+
+            for(int i = deckSize-1; i > 1; i--)
+            {
+                int j = -1; // j here is just an index variable, like i
+
+                while(!(0 <= j && j <= i)) // 0 ≤ j ≤ i
+                {
+                    // generate a random number for j until it falls between
+                    // 0 and i, inclusive
+                    rand.Next(0, deckSize + 1);
+                }
+                // swap positions of the two cards
+                CardModel temp = deck.Cards[i];
+                deck.Cards[i] = deck.Cards[j];
+                deck.Cards[j] = temp;
+            }
+            return deck;
+        }
+        /// <summary>
+        /// Fills a deck with 52 cards, unshuffled.
+        /// </summary>
+        public static Deck FillDeckWithUnshuffledCards(Deck deck)
+        {
+            foreach (CardSuitEnum suit in Enum.GetValues(typeof(CardSuitEnum)))
+            {
+                foreach (CardFaceValueEnum faceValue in Enum.GetValues(typeof(CardFaceValueEnum)))
+                {
+                    Color color;
+                    switch (suit)
+                    {
+                        case CardSuitEnum.CLUBS:
+                            color = Color.Black;
+                            break;
+                        case CardSuitEnum.SPADES:
+                            color = Color.Black;
+                            break;
+                        case CardSuitEnum.HEARTS:
+                            color = Color.Black;
+                            break;
+                        case CardSuitEnum.DIAMONDS:
+                            color = Color.Black;
+                            break;
+                        default:
+                            color = Color.Green;
+                            break;
+                    }
+                    deck.Cards.Add(new CardModel(color, suit, faceValue));
+                }
+            }
+            return deck;
+        }
+        /// <summary>
+        /// Merge two or more decks together.
+        /// <br></br>
+        /// This method works by merging all the cards of the decks into the first deck of the list.
+        /// </summary>
+        public static Deck MergeDecks(List<Deck> decks)
+        {
+            // Start at index 1 on purpose
+            for(int i = 1; i < decks.Count; i++)
+            {
+                foreach(CardModel card in decks[i].Cards)
+                {
+                    decks[0].Cards.Add(card);
+                }
+            }
+            return decks[0];
+        }
+    }
+}
---
+++ XamarinBlackjack/Tools/HandTools.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using XamarinBlackjack.Enums;
+using XamarinBlackjack.Models;
+
+namespace XamarinBlackjack.Tools
+{
+    public static class HandTools
+    {
+        /// <summary>
+        /// Calculates the numeric value of a Hand of Cards.
+        /// </summary>
+        public static int CalculateHandValue(HandModel hand)
+        {
+            /* This method handles one of the more interesting and fun (read: frustrating)
+             * challenges of programming a Blackjack game: "How do you programmatically
+             * calculate the value of a hand?"
+             * 
+             * In particular, how do you treat the value of an Ace?
+             * 
+             * The approach taken here is to always treat an Ace as a one (1) if the
+             * value of the hand is 10 or more. The reason for this is that if we were to
+             * treat it as an 11 instead of a 1 in that situation, then the hand would
+             * be a bust (it would go over 21 and lose).
+             * 
+             * When the value of the rest of the cards is LESS than 10, then
+             * we treat an Ace as an eleven (11).
+             */
+
+            int handValue = 0;
+            int numberOfAces = 0;
+            foreach (var card in hand.Cards)
+            {
+                switch (card.FaceValue)
+                {
+                    case CardFaceValueEnum.ACE:
+                        numberOfAces += 1;
+                        break;
+
+                    case CardFaceValueEnum.TWO:
+                        handValue += 2;
+                        break;
+
+                    case CardFaceValueEnum.THREE:
+                        handValue += 3;
+                        break;
+
+                    case CardFaceValueEnum.FOUR:
+                        handValue += 4;
+                        break;
+
+                    case CardFaceValueEnum.FIVE:
+                        handValue += 5;
+                        break;
+
+                    case CardFaceValueEnum.SIX:
+                        handValue += 6;
+                        break;
+
+                    case CardFaceValueEnum.SEVEN:
+                        handValue += 7;
+                        break;
+
+                    case CardFaceValueEnum.EIGHT:
+                        handValue += 8;
+                        break;
+
+                    case CardFaceValueEnum.NINE:
+                        handValue += 9;
+                        break;
+
+                    case CardFaceValueEnum.TEN:
+                        handValue += 10;
+                        break;
+
+                    case CardFaceValueEnum.JACK:
+                        handValue += 10;
+                        break;
+
+                    case CardFaceValueEnum.QUEEN:
+                        handValue += 10;
+                        break;
+
+                    case CardFaceValueEnum.KING:
+                        handValue += 10;
+                        break;
+
+                    default:
+                        break;
+                }
+                if (numberOfAces > 0)
+                {
+                    bool isHandValueGreaterThan9 = handValue > 9 ? true : false;
+                    for (int i = numberOfAces; i > 0; i--)
+                    {
+                        if (isHandValueGreaterThan9)
+                        {
+                            handValue++;
+                            continue;
+                        }
+                        if (!isHandValueGreaterThan9)
+                        {
+                            handValue += 11;
+                        }
+                    }
+                }
+            }
+            return handValue;
+        }
+    }
+}
